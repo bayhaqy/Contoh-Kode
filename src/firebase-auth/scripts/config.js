ffd057bc708e1d3294b292edd407870fefd1b29f
Yasya El Hakim <yasyaelhakim@gmail.com>
2022-11-30 19:17:54 +0700

    update 3 files and create 3 files

---
+++ src/firebase-auth/scripts/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBvx6U_qgSu4uyHwwla5DKzhw5JqevtjIw",
+    authDomain: "elcreative-login.firebaseapp.com",
+    projectId: "elcreative-login",
+    storageBucket: "elcreative-login.appspot.com",
+    messagingSenderId: "461246072776",
+    appId: "1:461246072776:web:a38923b07067feb61d22fd"
+};
+var pageDashboard = 'index.html';
+var container = document.querySelector('.app');
+var signInButton = document.body;
